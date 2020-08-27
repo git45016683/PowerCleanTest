9693c8a99fb08c938d969ed37785e42331c53b2b
icemaple <450196683@qq.com>
2020-08-28 00:53:59 +0800

    提交

--- AGV_Version/AGV_Version/Core/Src/main.c
+++ AGV_Version/AGV_Version/Core/Src/main.c
@@ -26,7 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include<stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -52,12 +52,39 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+//#ifdef __GNUC__
+//	#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+//#else
+//	#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+//#endif
+//PUTCHAR_PROTOTYPE
+//{
+//		HAL_UART_Transmit(&huart2 , (uint8_t *)&ch, 1, 0xFFFF);
+//		return ch;
+//}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define rx_size 2
+extern DMA_HandleTypeDef hdma_usart2_rx;
+extern DMA_HandleTypeDef hdma_usart2_tx;
+extern DMA_HandleTypeDef hdma_usart3_rx;
+extern DMA_HandleTypeDef hdma_usart3_tx;
+void ResetUserTimer(uint32_t *Timer)
+{
+	*Timer = HAL_GetTick();
+}
 
+uint32_t ReadUserTimer(uint32_t *Timer)
+{
+	return ( HAL_GetTick() - *Timer);
+}
+
+void SetUserTimer(uint32_t *Timer,uint32_t T)
+{
+	*Timer = HAL_GetTick() + T;
+}
 /* USER CODE END 0 */
 
 /**
@@ -67,7 +94,12 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	// 发送测试字符串
+	uint8_t str[] = "\r\n-------------Running Started------------------\r\n";
+	// 接收缓存区大小为20
+	uint8_t recvStr2[20] = {0x00};
+	uint8_t recvStr3[rx_size] = {0x99, 0xaa};
+	uint8_t sendStr3[rx_size] = {0x88, 0x00};
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -93,7 +125,19 @@ int main(void)
   MX_USART3_UART_Init();
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-
+//	HAL_UART_Transmit_DMA(&huart3, str, sizeof(str) - 1); 
+//  HAL_Delay(1000);
+//	HAL_UART_Receive_DMA(&huart3, (uint8_t *)recvStr3, rx_size);
+//	HAL_Delay(1000);
+	HAL_UART_Transmit_DMA(&huart2, str, sizeof(str) - 1); 
+  HAL_Delay(1000);
+	HAL_UART_Receive_DMA(&huart2, recvStr2, 20);
+//	printf("%s\r\n",str);
+//	for(int i=0; i<sizeof(recvStr3); i++)
+//	{
+//		printf("0x%02x ", recvStr3[i]);
+//	}
+//	printf("\r\n");
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -103,6 +147,58 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+		
+		static uint32_t uart3timer = 0;
+		if (ReadUserTimer(&uart3timer) > 1000)
+		{
+			ResetUserTimer(&uart3timer);
+//			//串口3接收
+////			printf("\r\n uart3_rxdma_state: %d", hdma_usart3_rx.State);
+//			if(hdma_usart3_rx.State == HAL_DMA_STATE_READY)
+//			{
+//				// 将接收到的字符打印出来进行观察
+//				// 发到串口3
+//				printf("\r\n receive data: ");
+//				for(int i=0; i<sizeof(recvStr3); i++)
+//				{
+//					printf("0x%02x ", recvStr3[i]);
+//				}
+//				printf(".\r\n");
+//				HAL_UART_Transmit_DMA(&huart3, sendStr3, sizeof(sendStr3) - 1);
+//				// 清除缓存区内容，方便进行下次接收
+//				memset(recvStr3,0,sizeof(recvStr3));
+//				// 软件将标志位清零
+//				hdma_usart3_rx.State = HAL_DMA_STATE_BUSY;
+//				// 继续继续下一回合的DMA接收，因为采用的非循环模式，再次调用会再次使能DMA
+//				HAL_UART_Receive_DMA(&huart3, (uint8_t *)recvStr3, rx_size);
+//				sendStr3[1]+=0x01;
+//			}
+			
+			//串口2接收
+//			printf("\r\n uart2_rxdma_state: %d", hdma_usart2_rx.State);
+			if(hdma_usart2_rx.State == HAL_DMA_STATE_READY)
+			{
+				// 将接收到的字符打印出来进行观察
+				// 发到串口2
+				HAL_UART_Transmit_DMA(&huart2, recvStr2, sizeof(recvStr2) - 1);
+				HAL_Delay(1000);
+				// 清除缓存区内容，方便进行下次接收
+				memset(recvStr2,0,sizeof(recvStr2));
+				// 软件将标志位清零
+				hdma_usart2_rx.State = HAL_DMA_STATE_BUSY;
+				// 继续继续下一回合的DMA接收，因为采用的非循环模式，再次调用会再次使能DMA
+				HAL_UART_Receive_DMA(&huart2, recvStr2, 20);
+			}
+		}
+		
+		static uint32_t lifetimer, lifeCount = 0;
+		if (ReadUserTimer(&lifetimer) > 1000*10)
+		{
+			ResetUserTimer(&lifetimer);
+//			printf("\r\nrunning %d(0) s", lifeCount++);
+			uint8_t runn[10] = "running";
+			HAL_UART_Transmit_DMA(&huart2, runn, sizeof(runn) - 1);
+		}
   }
   /* USER CODE END 3 */
 }
